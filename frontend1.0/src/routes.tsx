d93d8a593f7e21ee67a6f76f5cd06e48655fc1cd
hemanth18 <hg510443@gmail.com>
2025-03-20 20:19:01 +0530

    Updated routes for frontend

--- frontend1.0/src/routes.tsx
+++ frontend1.0/src/routes.tsx
@@ -1,16 +1,25 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { LoginPage } from "./pages/LoginPage";
-import { AuthProvider } from "./context/AuthContext";
-import { SignupPage } from "./pages/SignupPage";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { DashboardPage } from './pages/Dashboard';
+import { ProtectedRoute } from './components/Protected';
+import { AuthProvider } from './context/AuthContext';
+import { SignupPage } from './pages/SignupPage';
+import DocumentViewer from './components/DocumentViewer';
 
 export function AppRoutes() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          {/* Public Route */}
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage></SignupPage>}></Route>
+          <Route path='/' element={<Navigate to={'/login'} />}></Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/documents/:id" element={<DocumentViewer />} />
+          </Route>
 
           {/* Catch-all route */}
           <Route path="*" element={<Navigate to="/" />} />
